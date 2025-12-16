be8a7f73a6542542b6ee47173aac3eae0b86e85d
zawsainan <zawsainan@gmail.com>
2025-12-16 20:22:12 +0630

    added remove logic

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,25 +1,59 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 export default function Home() {
   const [ingredients, setIngredient] = useState([]);
+  const inputRef = useRef(null);
+  function handleSubmit(e) {
+    e.preventDefault();
+    let value = inputRef.current.value;
+    if (!value) {
+      return;
+    }
+    setIngredient((cur) => [...cur, value]);
+    inputRef.current.value = "";
+  }
 
+  function handleClick(indexToRemove) {
+    setIngredient((cur) => cur.filter((_, index) => index !== indexToRemove));
+  }
   return (
     <div className="flex flex-col mx-auto max-w-3xl p-4">
-      <div className="flex pt-4">
-        <input
-          type="text"
-          placeholder="eg. oregano"
-          className="grow border-[#6B7280] rounded-sm border-2 p-2"
-        />
-        <button className="w-48 text-center bg-[#141413] text-white py-2 rounded-sm ms-4">
-          + Add Ingredient
-        </button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="flex pt-4">
+          <input
+            type="text"
+            ref={inputRef}
+            placeholder="eg. oregano"
+            className="grow border-[#6B7280] rounded-sm border-2 p-2"
+          />
+          <button
+            type="submit"
+            className="w-48 text-center bg-[#141413] text-white py-2 rounded-sm ms-4"
+          >
+            + Add Ingredient
+          </button>
+        </div>
+      </form>
 
-      <h2 className="font-semibold text-xl mt-8 mb-4">Ingredients on hand:</h2>
-
-      <div className="min-h-25 border border-dashed p-4 text-gray-500">
-        (Ingredient list goes here)
-      </div>
+      {ingredients.length > 0 && (
+        <div className="min-h-25 p-4 text-gray-500">
+          <h2 className="font-semibold text-xl mt-8 mb-4">
+            Ingredients on hand:
+          </h2>
+          <ul className="space-y-3">
+            {ingredients.map((item, index) => (
+              <li className="flex justify-between" key={index}>
+                <span className="list-disc list-inside">{item}</span>
+                <button
+                  onClick={() => handleClick(index)}
+                  className="text-red-500 hover:text-red-700 text-sm"
+                >
+                  X
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
